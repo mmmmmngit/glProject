5189108e2d46785e10769696d2642e6c899bb1ad
mmmngit <tourokuyouf@gmail.com>
2019-11-06 16:26:42 +0900

    a

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+'use strict';
+
 window.addEventListener("load",()=>{
     console.log("a");
+    main();
 });
+
+function main(){
+    let gl = document.getElementById("canvas").getContext("webgl2");
+    if(gl)console.log("webgl loaded.")
+    else{
+        console.error("webgl2 has been deleted.");
+        return;
+    }
+
+    function render(){
+        gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
+        gl.flush();
+        window.requestAnimationFrame(render);
+    }
+}
