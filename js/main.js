2f434ae65242a781fc6ac4431ecbbb3aaec97fc4
mmmngit <tourokuyouf@gmail.com>
2019-11-07 14:44:50 +0900

    qa

--- js/main.js
+++ js/main.js
@@ -13,6 +13,27 @@ function main(){
         return;
     }
 
+    let vshader=`
+        attribute vec3 position;
+        attribute vec3 normal;
+        attribute vec2 texCoord;
+        
+        uniform mat4 mMatrix;
+        uniform mat4 mvpMatrix;
+        uniform mat4 normalMatrix;
+        
+        varying vec3 vPosition;
+        varying vec3 vNormal;
+        varying vec2 vTexCoord;
+        
+        void main(){
+            vPosition = (mMatrix * vec4(position, 1.0)).xyz;
+            vNormal = (normalMatrix * vec4(normal, 0.0)).xyz;
+            vTexCoord = texCoord;
+            gl_Position = mvpMatrix * vec4(position, 1.0);
+        }
+    `;
+
     function render(){
         gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
         gl.flush();
