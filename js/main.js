620ce3ece9ce3364d7e95cc791c995af6dc5d4d9
mmmngit <tourokuyouf@gmail.com>
2019-11-13 14:44:32 +0900

    ok

--- js/main.js
+++ js/main.js
@@ -5,22 +5,20 @@ window.addEventListener("load",()=>{
 
 function main(){
     const canvas = document.getElementById("canvas");
-    canvas.width = 200;
-    canvas.height = 200;
+    const width = 512,height=512;
+    canvas.width = width;
+    canvas.height = height;
     const gl = canvas.getContext("webgl2");
     if(gl)console.log("webgl2 loaded.")
     else{
         console.error("webgl2 has been deleted.");
         return;
     }
-    gl.clearColor(0.3, 0.3, 0.3, 1.0);
-    gl.clearDepth(1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    gl.enable(gl.BLEND);
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
+    //gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    //gl.enable(gl.BLEND);
+    //gl.enable(gl.DEPTH_TEST);
+    //gl.depthFunc(gl.LEQUAL);
 
     const VERTEX_SIZE = 3;
     const COLOR_SIZE = 4;
@@ -30,12 +28,12 @@ function main(){
 
         in vec3 vPosition;
         in vec4 color;
-        
+
         out vec4 vColor;
         
         void main() {
-        vColor = color;
-        gl_Position = vec4(vPosition, 1.0);
+            vColor = color;
+            gl_Position = vec4(vPosition, 1.0);
         }
     `;
     const fShaderSource
@@ -43,84 +41,131 @@ function main(){
         precision highp float;
 
         in vec4 vColor;
+        uniform float timer;
+
         out vec4 fColor;
 
         void main() {
-        fColor = vColor;
+            vec3 t = gl_FragCoord.xyz;
+            fColor = vec4(t.x/400.0,t.y/400.0,t.z+timer/10.0,1.0);
         }
     `;
 //compile
-    const vShader = gl.createShader(gl.VERTEX_SHADER);
-    const fShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(vShader, vShaderSource);
-    gl.compileShader(vShader);
-    if(!gl.getShaderParameter(vShader, gl.COMPILE_STATUS))console.log(gl.getShaderInfoLog(vShader));
-    gl.shaderSource(fShader, fShaderSource);
-    gl.compileShader(fShader);
-    if(!gl.getShaderParameter(fShader, gl.COMPILE_STATUS))console.log(gl.getShaderInfoLog(fShader));
+    const vs = create_shader(vShaderSource,"vertex");
+    const fs = create_shader(fShaderSource,"fragment")
 //link
-    const program = gl.createProgram();
-    gl.attachShader(program, vShader);
-    gl.attachShader(program, fShader);
-    gl.linkProgram(program);
-    if(!gl.getProgramParameter(program, gl.LINK_STATUS))console.log(gl.getProgramInfoLog(program));
+    const program = create_program(vs,fs);
     gl.useProgram(program);
-//buffer
-    const vBuffer = gl.createBuffer();
-    const cBuffer = gl.createBuffer();
-    const iBuffer = gl.createBuffer();
-    const vAttLocation = gl.getAttribLocation(program, 'vPosition');
-    const cAttLocation  = gl.getAttribLocation(program, 'color');
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.enableVertexAttribArray(vAttLocation);
-    gl.vertexAttribPointer(vAttLocation, VERTEX_SIZE, gl.FLOAT, false, 0, 0);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.enableVertexAttribArray(cAttLocation);
-    gl.vertexAttribPointer(cAttLocation, COLOR_SIZE, gl.FLOAT, false, 0, 0);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
 //data
-    const vData = new Float32Array([
+    const vData =[
         1,1,0,
         -1,1,0,
         1,-1,0,
         -1,-1,0,
-    ]);
-    const cData = new Float32Array([
+    ];
+    const cData = [
         1.0, 0.0, 0.0, 1.0,
         0.0, 1.0, 0.0, 1.0,
         0.0, 0.0, 1.0, 1.0,
         0.0, 0.0, 0.0, 1.0
-    ]);
-    const iData = new Uint32Array([
+    ];
+    const iData = [
         0,1,2,
-        1,3,2
-    ]);
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vData, gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        2,3,1
+    ];
+    const attLocation = [
+        gl.getAttribLocation(program, 'vPosition'),
+        gl.getAttribLocation(program, 'color')
+    ];
+    const attStride=[3,4];
+    const vao = create_vao([vData,cData],attLocation,attStride,iData);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, cData, gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, iData, gl.STATIC_DRAW);
-    
+    gl.bindVertexArray(vao);
     gl.drawElements(gl.TRIANGLES, iData.length, gl.UNSIGNED_SHORT, 0);
     gl.flush();
 
-    //render();
+    window.requestAnimationFrame(render);
+
+    let r=0,g=0,b=0;
     function render(){
-        gl.clearColor(0.3, 0.3, 0.3, 1.0);
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.clearDepth(1.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.viewport(0, 0, width, height);
 
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+        gl.bindVertexArray(vao);
         gl.drawElements(gl.TRIANGLES, iData.length, gl.UNSIGNED_SHORT, 0);
         gl.flush();
         window.requestAnimationFrame(render);
     }
+    //////////////////////////////////////////////////////////////////////////////////////////
+    function create_shader(source,type){
+        var shader;
+        var script = source;
+        if(!source){return;}
+        switch(type){
+            case 'vertex':
+                shader = gl.createShader(gl.VERTEX_SHADER);
+                break;
+            case 'fragment':
+                shader = gl.createShader(gl.FRAGMENT_SHADER);
+                break;
+            default :
+                return;
+        }
+        gl.shaderSource(shader, script);
+        gl.compileShader(shader);
+        if(gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+            return shader;
+        }else{
+            console.error(gl.getShaderInfoLog(shader));
+        }
+    }
+    function create_program(vs, fs){
+        var program = gl.createProgram();
+        gl.attachShader(program, vs);
+        gl.attachShader(program, fs);
+        gl.linkProgram(program);
+        if(gl.getProgramParameter(program, gl.LINK_STATUS)){
+            gl.useProgram(program);
+            return program;
+        }else{
+            alert(gl.getProgramInfoLog(program));
+        }
+    }
+    function create_vbo(data){
+        var vbo = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        return vbo;
+    }
+    function create_ibo(data){
+        var ibo = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+        return ibo;
+    }
+    function create_vao(vboDataArray, attLocation, attStride, iboData){
+        var vao, vbo, ibo, i;
+        vao = gl.createVertexArray();//vao作成
+        gl.bindVertexArray(vao);
+        for(i in vboDataArray){//vbo作成
+            vbo = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vboDataArray[i]), gl.STATIC_DRAW);
+            gl.enableVertexAttribArray(attLocation[i]);
+            gl.vertexAttribPointer(attLocation[i], attStride[i], gl.FLOAT, false, 0, 0);
+        }
+        if(iboData){//ibo作成
+            ibo = gl.createBuffer();
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(iboData), gl.STATIC_DRAW);
+        }
+        gl.bindVertexArray(null);
+        return vao;
+    }
 }
+
