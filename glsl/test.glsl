c57276ac285e62b687419d41376f47ca59370a07
mmmngit <tourokuyouf@gmail.com>
2019-12-02 21:12:51 +0900

    a

--- glsl/test.glsl
+++ glsl/test.glsl
@@ -36,45 +36,47 @@ float sd_box( vec3 p, vec3 b )
 
 
 vec3 op_trans(vec3 p){
-    return mod(p, 8.0) - 4.0;
+    float t=10.0;
+    return mod(p, t) - t/2.0;
 }
 
 
 float distFunc(vec3 p){
-    float sp1 = sd_sphere(op_trans(p),2.0);
+    float sp1 = sd_sphere(op_trans(p),3.0*sin(u_time));
 
-    float box1= sd_box(op_trans(p),vec3(0.70817));
+    float box1= sd_box(op_trans(p),vec3(2.0));
 
-    float obj1 = max(sp1,box1);
-    return sp1;
+    float obj1 = min(sp1,box1);
+    return box1;
 }
 
 vec3 normal(vec3 p, float s ){
     float d = acc;
     return normalize(vec3(
-        distFunc(p + vec3(  d, 0.0, 0.0)) - distFunc(p + vec3( -d, 0.0, 0.0)),
-        distFunc(p + vec3(0.0,   d, 0.0)) - distFunc(p + vec3(0.0,  -d, 0.0)),
-        distFunc(p + vec3(0.0, 0.0,   d)) - distFunc(p + vec3(0.0, 0.0,  -d))
+        distFunc(p + vec3(  d, 0.0, 0.0)) - distFunc(p + vec3(0.0, 0.0, 0.0)),
+        distFunc(p + vec3(0.0,   d, 0.0)) - distFunc(p + vec3(0.0,  0.0, 0.0)),
+        distFunc(p + vec3(0.0, 0.0,   d)) - distFunc(p + vec3(0.0, 0.0,  0.0))
     ));
 }
 
 
 void main() {
     
-    const vec3 lightDir = vec3(0.3, 0.3, 1.0);
+    const vec3 lightDir = vec3(0.1, 0.2, 0.3);
     vec2 p = (gl_FragCoord.xy * 2.0 - u_resolution.xy) / min(u_resolution.x, u_resolution.y);
     
+    vec2 vec= vec2(sin(u_time)>0.0?sin(u_time)*50.0:0.0,sin(u_time)<0.0?sin(u_time)*50.0:0.0);
     Camera c;
-    c.pos = vec3(0.0,  0.0, 2.0);   // カメラの位置
+    c.pos = vec3(vec, 10.0*sin(u_time));   // カメラの位置
     c.dir = vec3(0.0,  0.0, -1.0);  // カメラの向き(視線)
-    c.up  = vec3(0.0,  1.0,  0.0);// カメラの上方向
+    c.up  = vec3(sin(u_time/3.0),  cos(u_time/1.0),  0.0);// カメラの上方向
     c.side = cross(c.dir, c.up);    // 外積を使って横方向を算出
     c.depth = 1.0;          // フォーカスする深度
-    c.fov = 60.0 * 0.5 * PI / 180.0;
+    c.fov = 120.0 * 0.5 * PI / 180.0;
 
     Ray r;
     r.pos = c.pos;
-    r.dir = sin(c.fov)*p.x*c.side+sin(c.fov)*p.y*c.up+cos(c.fov)*c.dir*c.depth;
+    r.dir = normalize(sin(c.fov)*p.x * c.side + sin(c.fov)*p.y * c.up + cos(c.fov)*c.dir*c.depth);
     
     float t=0.0,d;
     for(int i = 0; i < 64; i++){
